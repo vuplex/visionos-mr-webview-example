6c4d9cd01b0e5c156f17db55540fa6642f842648
binarynate <binarynate@gmail.com>
2024-08-15 19:40:02 -0500

    Updated WindowedWebViewExample.cs to provide an example of quitting the application with Application.Quit() if the Volume or webview window is closed, which prevents the app from continuing to run in the background.

--- Assets/Scripts/WindowedWebViewExample.cs
+++ Assets/Scripts/WindowedWebViewExample.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using UnityEngine;
+using Unity.PolySpatial;
 using Vuplex.WebView;
 
 /// <summary>
@@ -27,6 +28,24 @@ public class WindowedWebViewExample : MonoBehaviour {
         // Now that the IWebView is created, the app can use IWebView APIs like LoadUrl()
         // https://developer.vuplex.com/webview/IWebView
         webView.LoadUrl("https://www.google.com");
+
+        // If either the Volume or the webview window is closed, then quit the application with Application.Quit().
+        // This ensures that if the user re-opens the app from the visionOS home view, the app starts back up in the intended state.
+        // Otherwise, the default on visionOS is that the application will remain running in the background unless
+        // the user force quits it. If the app continues to run in the background and the user re-opens it from
+        // the visionOS home view, then visionOS will only reopen the window that was open last (either the Volume or the webview
+        // window, but not both). 
+        var volumeCamera = FindObjectOfType<VolumeCamera>();
+        volumeCamera.OnWindowEvent.AddListener(state => {
+            if (state.WindowEvent == VolumeCamera.WindowEvent.Closed) {
+                Application.Quit();
+            }
+        });
+        #if UNITY_VISIONOS && !UNITY_EDITOR
+            (webView as VisionOSWebView).WindowClosed += (sender, eventArgs) => {
+                Application.Quit();
+            };
+        #endif
     }
 
     async Task<IWebView> _createWebViewForEditor() {
